36761a25d9387b713b003d20f5e8bed5ccf0e4fe
vetasr <pavliy.vitaliy@gmail.com>
2015-08-17 13:01:26 +0300

    Finalized calculation of the position tooltip given the size of the downloaded images

--- js/jquery.simpletip-1.0.js
+++ js/jquery.simpletip-1.0.js
@@ -51,92 +51,94 @@
                   display: 'none'
                }).addClass(settings.class);
                
-               var currentImg = 0;
-               var totalImg = $('#SimpleTip img').bind('load error', function(){
-                  currentImg++;
-               }).size();
-               
-               var timer = setInterval(function(){
-                  if (currentImg == totalImg)
-                  {
-                     var widthMy       = $('#SimpleTip').outerWidth();
-                     var heightMy      = $('#SimpleTip').outerHeight();
+               var callback = function(){
+                  var widthMy       = $('#SimpleTip').outerWidth();
+                  var heightMy      = $('#SimpleTip').outerHeight();
                      
-                     var my      = settings.my.split(' ');
-                     var at      = settings.at.split(' ');
+                  var my      = settings.my.split(' ');
+                  var at      = settings.at.split(' ');
                
-                     my[0] = DecodeStr(my[0]); my[1] = DecodeStr(my[1]); 
-                     at[0] = DecodeStr(at[0]); at[1] = DecodeStr(at[1]); 
+                  my[0] = DecodeStr(my[0]); my[1] = DecodeStr(my[1]); 
+                  at[0] = DecodeStr(at[0]); at[1] = DecodeStr(at[1]); 
                
-                     var left;
-                     var top;
-                     var offsetx = settings.offsetx;
-                     var offsety = settings.offsety;
-                     var flag = false;
+                  var left;
+                  var top;
+                  var offsetx = settings.offsetx;
+                  var offsety = settings.offsety;
+                  var flag = false;
                
-                     for (var i=0; i<2; i++)
-                     {
-                        switch (at[0]){
-                           case 0: left = offset.left + offsetx; break;
-                           case 1: left = offset.left + offsetx + widthAt / 2;  break;
-                           case 2: left = offset.left + offsetx + widthAt; break;
-                        }
-               
-                        switch (at[1]){
-                           case 0: top = offset.top + offsety; break;
-                           case 1: top = offset.top + offsety + heightAt / 2; break;
-                           case 2: top = offset.top + offsety + heightAt; break;
-                        }
-               
-                        switch (my[0]){
-                           case 0: break;
-                           case 1: left -= widthMy / 2; break;
-                           case 2: left -= widthMy; break;
-                        }
-               
-                        switch (my[1]){
-                           case 0: break;
-                           case 1: top -= heightMy / 2; break;
-                           case 2: top -= heightMy; break;
-                        }
-         
-                        if (left + widthMy > widthWin + scrollLeftWin || left < scrollLeftWin)
-                        {
-                           my[0] = 2 - my[0];
-                           at[0] = 2 - at[0];
-                           offsetx = -offsetx;
-                           flag  = true;
-                        }
-                  
-                        if (top + heightMy > heightWin + scrollTopWin || top < scrollTopWin)
-                        {
-                           my[1] = 2 - my[1];
-                           at[1] = 2 - at[1];
-                           offsety = -offsety;
-                           flag  = true;
-                        }
-                  
-                        if (flag == false)
-                           break;
+                  for (var i=0; i<2; i++)
+                  {
+                     switch (at[0]){
+                        case 0: left = offset.left + offsetx; break;
+                        case 1: left = offset.left + offsetx + widthAt / 2;  break;
+                        case 2: left = offset.left + offsetx + widthAt; break;
+                     }
+                     
+                     switch (at[1]){
+                        case 0: top = offset.top + offsety; break;
+                        case 1: top = offset.top + offsety + heightAt / 2; break;
+                        case 2: top = offset.top + offsety + heightAt; break;
                      }
                
-                     if (left < scrollLeftWin)
-                        left = scrollLeftWin;
-                     if (top < scrollTopWin)
-                        top = scrollTopWin;  
-                     if (left > widthWin + scrollLeftWin - widthMy)
-                        left = widthWin + scrollLeftWin - widthMy;
-                     if (top > heightWin + scrollTopWin - heightMy)
-                        top = heightWin + scrollTopWin - heightMy;
+                     switch (my[0]){
+                        case 0: break;
+                        case 1: left -= widthMy / 2; break;
+                        case 2: left -= widthMy; break;
+                     }
                
-                     $('#SimpleTip').css({
-                        left: Math.round(left) + 'px',
-                        top:  Math.round(top)  + 'px'
-                     }).delay(settings.timedelay).fadeIn(settings.timefade);
-                     
-                     clearInterval(timer);
+                     switch (my[1]){
+                        case 0: break;
+                        case 1: top -= heightMy / 2; break;
+                        case 2: top -= heightMy; break;
+                     }
+         
+                     if (left + widthMy > widthWin + scrollLeftWin || left < scrollLeftWin)
+                     {
+                        my[0] = 2 - my[0];
+                        at[0] = 2 - at[0];
+                        offsetx = -offsetx;
+                        flag  = true;
+                     }
+                  
+                     if (top + heightMy > heightWin + scrollTopWin || top < scrollTopWin)
+                     {
+                        my[1] = 2 - my[1];
+                        at[1] = 2 - at[1];
+                        offsety = -offsety;
+                        flag  = true;
+                     }
+                  
+                     if (flag == false)
+                        break;
                   }
-               }, 10);
+               
+                  if (left < scrollLeftWin)
+                     left = scrollLeftWin;
+                  if (top < scrollTopWin)
+                     top = scrollTopWin;  
+                  if (left > widthWin + scrollLeftWin - widthMy)
+                     left = widthWin + scrollLeftWin - widthMy;
+                  if (top > heightWin + scrollTopWin - heightMy)
+                     top = heightWin + scrollTopWin - heightMy;
+            
+                  $('#SimpleTip').css({
+                     left: Math.round(left) + 'px',
+                     top:  Math.round(top)  + 'px'
+                  }).delay(settings.timedelay).fadeIn(settings.timefade);
+               }
+                           
+               var currentImg = 0;
+               var totalImg = $('#SimpleTip img').size();
+               
+               if (totalImg == 0)
+                  callback();
+               else
+                  $('#SimpleTip img').bind('load error', function(){
+                     currentImg++;
+                     if (currentImg == totalImg)
+                        callback();
+                  });
             });
             
             $(this).mouseout(function(){
